ce44b450a0774e1de411f331f8d14db91c017ed3
kareem wael <100386920+Kareemelshekh11@users.noreply.github.com>
2022-09-03 21:13:40 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import 'slick-carousel/slick/slick.min.js';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-import { BrowserRouter ,Routes , Route} from 'react-router-dom';
+import { BrowserRouter ,Routes , Route , HashRouter } from 'react-router-dom';
 import About from './Components/about';
 import Home from './Components/home';
 import OurTeam from './Components/ourteam';
@@ -23,26 +23,25 @@ import Contact from './Components/contact';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <Routes>
       <Route path='' element={<Home/>}/>
-      <Route path='/reactpro' element={<Home/>}/>
       <Route path='/' element={<Home/>}/>
-      <Route path='#home' element={<Home/>}/>
-      <Route path='#about' element={<About/>}/>
-      <Route path='#ourteam' element={<OurTeam/>}/>
-      <Route path='#faqs' element={<FaqS/>}/>
-      <Route path='#booking' element={<Booking/>}/>
-      <Route path='#notfound' element={<NotFound/>}/>
+      <Route path='/home' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/ourteam' element={<OurTeam/>}/>
+      <Route path='/faqs' element={<FaqS/>}/>
+      <Route path='/booking' element={<Booking/>}/>
+      <Route path='/notfound' element={<NotFound/>}/>
       <Route path='*' element={<NotFound/>}/>
-      <Route path='#login' element={<Login/>}/>
-      <Route path='#service' element={<Service/>}/>
-      <Route path='#servicedetails' element={<ServiceDetails/>}/>
-      <Route path='#blog' element={<Blog/>}/>
-      <Route path='#blogdetails' element={<BlogDetails/>}/>
-      <Route path='#contact' element={<Contact/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/service' element={<Service/>}/>
+      <Route path='/servicedetails' element={<ServiceDetails/>}/>
+      <Route path='/blog' element={<Blog/>}/>
+      <Route path='/blogdetails' element={<BlogDetails/>}/>
+      <Route path='/contact' element={<Contact/>}/>
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
