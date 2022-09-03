796f3a92e181f2ad76a3054cc65fa09f7adfeff8
kareem wael <100386920+Kareemelshekh11@users.noreply.github.com>
2022-09-03 20:54:10 +0200

    Add files via upload

--- src/Sections/navbar.js
+++ src/Sections/navbar.js
@@ -30,39 +30,39 @@ function NavBars(){
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="#home">Home</Nav.Link>
             <NavDropdown title="Pages" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/about">About Us</NavDropdown.Item>
-              <NavDropdown.Item href="/ourteam">
+              <NavDropdown.Item href="#about">About Us</NavDropdown.Item>
+              <NavDropdown.Item href="#ourteam">
                 Our Team
               </NavDropdown.Item>
-              <NavDropdown.Item href="/faqs">FAQ's</NavDropdown.Item>
-              <NavDropdown.Item href="/booking">
+              <NavDropdown.Item href="#faqs">FAQ's</NavDropdown.Item>
+              <NavDropdown.Item href="#booking">
                 Booking
               </NavDropdown.Item>
-              <NavDropdown.Item href="/notfound">
+              <NavDropdown.Item href="#notfound">
                 Error 404
               </NavDropdown.Item>
-              <NavDropdown.Item href="/login">
+              <NavDropdown.Item href="#login">
                 Login / Register
               </NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="Services" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/service">Service</NavDropdown.Item>
-              <NavDropdown.Item href="/servicedetails">
+              <NavDropdown.Item href="#service">Service</NavDropdown.Item>
+              <NavDropdown.Item href="#servicedetails">
                 Service Details
               </NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="Blog" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/blog">Blogs</NavDropdown.Item>
-              <NavDropdown.Item href="/blogdetails">
+              <NavDropdown.Item href="#blog">Blogs</NavDropdown.Item>
+              <NavDropdown.Item href="#blogdetails">
                 Blog Details
               </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="/Contact">Contact Us</Nav.Link>
+            <Nav.Link href="#Contact">Contact Us</Nav.Link>
             <Nav.Link> <FaSearch/> </Nav.Link>
             <Nav.Link href="#Contact">
-                <a href='/contact'><button type='button'>Contact Us <span> <IoIosArrowForward /> </span></button></a>
+                <a href='#contact'><button type='button'>Contact Us <span> <IoIosArrowForward /> </span></button></a>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
